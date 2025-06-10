fda31dfccb90580b90b0adf7be51d4697fbf5418
Barnik Chakraborty <barnikc79@gmail.com>
2025-06-10 11:32:10 +0530

    Program to print the spiral matrix

---
+++ Medium/SpiralMatrix.cpp
@@ -0,0 +1,86 @@
+/**
+ * Given a Matrix, print the given matrix in spiral order.
+ * 
+ * Example 1:
+ * Input: Matrix[][] = { { 1, 2, 3, 4 },
+ *             	         { 5, 6, 7, 8 },
+ *                       { 9, 10, 11, 12 },
+ *                       { 13, 14, 15, 16 } }
+ * 
+ * Output: 1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10.
+ * Explanation: The output of matrix in spiral form.
+ * 
+ * Example 2:
+ * Input: Matrix[][] = { { 1, 2, 3 },
+ *                       { 4, 5, 6 },
+ *                       { 7, 8, 9 } }
+ *  Output: 1, 2, 3, 6, 9, 8, 7, 4, 5.
+ * Explanation: The output of matrix in spiral form.
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> printSpiral(vector<vector<int>> &matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    //Initializing pointers to keep track
+    //of the matrix
+    int top = 0, bottom = n-1;
+    int left = 0, right = m-1;
+
+    vector<int> ans; //vector to store the output
+
+    while(top <= bottom && left <= right) {
+        for(int i=left;i<=right;i++) {
+            ans.push_back(matrix[top][i]);
+        }
+        top++;
+        for(int i=top;i<=bottom;i++) {
+            ans.push_back(matrix[i][right]);
+        }
+        right--;
+        if(top<=bottom) {
+            for(int i=right;i>=left;i--) {
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        if(left<=right) {
+            for(int i=bottom;i>=top;i--) {
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int n,m;
+    cout << "Enter the number of rows and columns : ";
+    cin >> n >> m;
+
+    vector<vector<int>> matrix;
+
+    cout << "Enter the elements row-wise : " << endl;
+    for(int i=0;i<n;i++) {
+        vector<int> vec;
+        for(int j=0;j<m;j++) {
+            int temp;
+            cin >> temp;
+            vec.push_back(temp);
+        }
+        matrix.push_back(vec);
+    }
+
+    vector<int> spiral = printSpiral(matrix);
+
+    cout << "The spiral is : " << endl;
+    for(int i=0;i<spiral.size();i++) {
+        cout << spiral[i] << " ";
+    }
+    return 0;
+}
